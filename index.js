71415354a01d8a2c6f923cff2ebc341bb63a647f
ChengFu Lin <chengfulin0806@gmail.com>
2017-05-27 04:03:04 +0800

    Use linebot instead

--- index.js
+++ index.js
@@ -1,26 +1,19 @@
 const express = require('express');
-const crypto = require('crypto');
 const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const line = require('@line/bot-sdk');
+const linebot = require('linebot');
 
-const channelSecret = 'f2b0bd7ed2faf7f3fcddc58a67306561'; // Channel secret string
+const bot = linebot({
+    channelId: '1517058705',
+    channelSecret: 'f2b0bd7ed2faf7f3fcddc58a67306561',
+    channelAccessToken: 'poBOF4jaxKKd8zfQjz79UqBNK1U69117TE1d80T75cE1G8ZYKeS/5pdGSqoanGNppBP717mlFuE/TE0sDvs6qER0Y7Uwt1TH/oouJ0VFMKGM6cysqnDjLL0Do1hgT9FjrqkLkp0q4YLj8SUWw3KY6wdB04t89/1O/w1cDnyilFU='
+});
 
 const app = express();
+const linebotParser = bot.parser();
 app.use(morgan('dev')); // log every request on console
 app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.json()); // parsing application/json
 
-app.post('/', (req, res) => {
-    const signature = crypto.createHmac('SHA256', channelSecret)
-        .update(JSON.stringify(req.body))
-        .digest('base64');
-    if (req.get('X-Line-Signature') === signature) {
-        res.status(200).send({});
-    }
-    else
-        res.status(500).send({});
-});
+app.post('/', linebotParser);
 
 app.listen(process.env.PORT || 8080, () => {
     console.log('> server is running on port' + process.env.PORT || 8080);
