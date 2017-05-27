a03f77eabc072ed18f76d7feb80193a788dec498
ChengFu Lin <chengfulin0806@gmail.com>
2017-05-27 14:23:37 +0800

    Create notification

--- TextMessageHandler.js
+++ TextMessageHandler.js
@@ -1,15 +1,14 @@
 const drugsInfo = require('./drugs.json');
 const base64 = require('node-base64-image');
+const Notification = require("./app/models/Notification");
 
 class TextMessageHandler{
     constructor() {
         this._keywords = {
-            echo: /^##\s+/,
-            drugInfo: /^#druginfo\s+/
+            echo: /^##\s+/
         };
         this._pattern = {
-            echo: /^##\s+.+/,
-            drugInfo: /^#druginfo\s+.+/
+            echo: /^##\s+.+/
         };
     }
 
@@ -21,14 +20,17 @@ class TextMessageHandler{
         if (this._pattern.echo.test(event.message.text)) {
             this.processEcho(event);
         }
-        else if (this._pattern.drugInfo.test(event.message.text)) {
+        else if (event.message.type === 'text') {
             this.processDrugInfo(event);
+        }
+        else if (event.message.type === 'location') {
+            this.processNotification(event);
         }
     }
 
     processEcho(event) {
         const output = event.message.text.substring(event.message.text.match(this._keywords.echo)[0].length);
-        event.reply(output)
+        event.reply(event.message.text)
         .then(function (data) {
             // success 
         })
@@ -38,44 +40,43 @@ class TextMessageHandler{
     }
 
     processDrugInfo(event) {
-        const search = event.message.text.substring(event.message.text.match(this._keywords.drugInfo)[0].length);
         let info = "";
         let foundDrugName = "";
-        let foundDrugImg = "";
         for (let index = 0; index < drugsInfo.length; ++index) {
             const names = drugsInfo[index][1]['藥物名稱'] + drugsInfo[index][2]['俗名'];
-            if (names.indexOf(search) !== -1) {
+            if (names.indexOf(event.message.text) !== -1) {
                 info = drugsInfo[index][6]['說明'];
                 foundDrugName = drugsInfo[index][1]['藥物名稱'];
-                foundDrugImg = encodeURI(drugsInfo[index][7]['圖片'].split(';')[0]);
                 break;
             }
         };
         // console.log(foundDrugImg);
         if (info && info.length > 0) {
             event.reply(info)
-            // event.reply({
-            //     'type': 'template',
-            //     'altText': foundDrugName,
-            //     'template': {
-            //         'type': 'buttons',
-            //         'title': '管制藥品資訊',
-            //         'text': 'info',
-            //         'actions': []
-            //     }
-            // })
-            // event.reply({
-            //     "type": "image",
-            //     "originalContentUrl": foundDrugImg,
-            //     "previewImageUrl": foundDrugImg
-            // })
-            .then((data) => {
-                console.log(">> template success");
-                console.log(data);
-            })
-            .catch(error => console.log(error.message));
+                .then((data) => {
+                    console.log(">> template success");
+                    console.log(data);
+                })
+                .catch(error => console.log(error.message));
         }
         else event.reply("抱歉！沒有您找的管制藥品。");
+    }
+
+    processNotification(event) {
+        let notification = new Notification();
+        notification.time = new Date(event.timestamp);
+        notification.address = event.message.address || "";
+        notification.latitude = event.message.latitude;
+        notification.longitude = event.message.longitude;
+        notification.save()
+            .then(() => {
+                console.log('> new notification');
+                event.reply('通報成功');
+            })
+            .catch((error) => {
+                console.log(error.message);
+                event.reply('通報失敗');
+            });
     }
 }
 
