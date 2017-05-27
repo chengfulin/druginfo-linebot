f42deb982c11f193dd4bdcea6dc46df6caea4214
ChengFu Lin <chengfulin0806@gmail.com>
2017-05-27 10:16:45 +0800

    Try get drug opendata

--- TextMessageHandler.js
+++ TextMessageHandler.js
@@ -1,11 +1,14 @@
+var fetch = require("node-fetch");
 
 class TextMessageHandler{
     constructor() {
         this._keywords = {
-            echo: /^##\s+/
+            echo: /^##\s+/,
+            drugInfo: /^#druginfo\s+/
         };
         this._pattern = {
-            echo: /^##\s+.+/
+            echo: /^##\s+.+/,
+            drugInfo: /^#druginfo\s+.+/
         };
     }
 
@@ -16,6 +19,9 @@ class TextMessageHandler{
     process(event) {
         if (this._pattern.echo.test(event.message.text)) {
             this.processEcho(event);
+        }
+        else if (this._pattern.drugInfo.test(event.message.text)) {
+            this.processDrugInfo(event);
         }
     }
 
@@ -28,6 +34,30 @@ class TextMessageHandler{
         .catch(function (error) {
             // error 
         });
+    }
+
+    processDrugInfo(event) {
+        const search = event.message.text.substring(event.message.text.match(this._keywords.drugInfo[0].length));
+        fetch('http://data.fda.gov.tw/cacheData/50_3.json')
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                const info = "";
+                data.forEach((drug) => {
+                    const names = drug[1]['藥物名稱'] + drug[2]['俗名'];
+                    if (names.indexOf(search) !== -1) {
+                        info = drug[6]['說明'];
+                        return false;
+                    }
+                });
+                return info;
+            })
+            .then((info) => {
+                if (!info || info.length === 0) return;
+                event.reply(info);
+            })
+            .catch(error => console.log(error.message));
     }
 }
 
