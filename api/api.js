08fb27bc8775887550206de68bcff0ad43b6116f
ChengFu Lin <chengfulin0806@gmail.com>
2017-05-27 15:17:27 +0800

    set response headers

--- api/api.js
+++ api/api.js
@@ -2,9 +2,9 @@ const router = require('express').Router();
 const Notification = require('../app/models/Notification');
 
 router.get('/notification/all', (req, res) => {
-    req.header('Access-Control-Origin', '*');
-    req.header('Access-Control-Methods', 'GET,PUT,DELETE,POST,OPTIONS');
-    req.header('Access-Control-Headers', 'X-Requested-With,Content-Type,Accept');
+    res.header('Access-Control-Origin', '*');
+    res.header('Access-Control-Methods', 'GET,PUT,DELETE,POST,OPTIONS');
+    res.header('Access-Control-Headers', 'X-Requested-With,Content-Type,Accept');
     Notification.find({})
         .then((notifications) => {
             res.json({ notifications: notifications });
