378e1a244bb7afd9256dd1423ddb6249e6a2350a
ChengFu Lin <chengfulin0806@gmail.com>
2017-05-28 09:14:35 +0800

    Add api for creation

--- api/api.js
+++ api/api.js
@@ -19,7 +19,7 @@ router.post('/notification/add', (req, res) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,DELETE,POST,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Accept');
-    console.log(req.body);
+
     if (!req.body.drug || req.body.drug.length === 0) {
         res.status(500).json({ error: '請輸入藥品名稱' });
         return;
@@ -33,8 +33,9 @@ router.post('/notification/add', (req, res) => {
         res.status(500).json({ error: '您輸入的藥品名稱未列舉' });
         return;
     }
-    console.log(req.body);
+
     let notification = new Notification();
+    notification.address = req.body.address;
     notification.latitude = req.body.latitude;
     notification.longitude = req.body.longitude;
     notification.drug = req.body.drug;
