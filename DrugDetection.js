4a1a04182945bacbac711269a5719b55d4cc5045
ChengFu Lin <chengfulin0806@gmail.com>
2017-05-29 14:22:32 +0800

    Try child_process python

--- DrugDetection.js
+++ DrugDetection.js
@@ -1,17 +1,20 @@
-var PythonShell = require('python-shell');
+const spawn = require('child_process').spawn;
+const py = spawn('python', ['./python/tf_files/label_image.py', 'morphine.jpg']);
 
 class DrugDetection {
 
     process() {
-        var options = {
-            args: ['morphine.jpg']
-        };
+        // var options = {
+        //     args: ['morphine.jpg']
+        // };
 
-        PythonShell.run('./python/tf_files/label_image.py', options, function (err, results) {
-            if (err) throw err;
-            // results is an array consisting of messages collected during execution
-            console.log('results: %j', results);
-        });
+        // PythonShell.run('./python/tf_files/label_image.py', options, function (err, results) {
+        //     if (err) throw err;
+        //     // results is an array consisting of messages collected during execution
+        //     console.log('results: %j', results);
+        // });
+        const py = spawn('python', ['./python/tf_files/label_image.py', 'morphine.jpg']);
+        console.log(py);
     }
 }
 
