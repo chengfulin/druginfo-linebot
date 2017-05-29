f471c8f717a32e4d6ad8e31744418b394860a4e2
ChengFu Lin <chengfulin0806@gmail.com>
2017-05-29 20:51:14 +0800

    drbug image detection

--- TextMessageHandler.js
+++ TextMessageHandler.js
@@ -255,7 +255,7 @@ class TextMessageHandler{
                     }
                 });
             })
-            .catch(error => console.log(error.mesage));
+            .catch(error => console.log(error));
     }
 }
 
