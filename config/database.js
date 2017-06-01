4aabfdb1f99947841c8ce5ee7742fba48190d1ff
ChengFu Lin <chengfulin0806@gmail.com>
2017-06-01 21:59:28 +0800

    hide

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose");
-var dbUri = "mongodb://meanadmin:R77K9ESG8TLY@ds059722.mlab.com:59722/mean276";
+var dbUri = "mongodb://[dbuser]:[password]@ds059722.mlab.com:59722/mean276";
 
 mongoose.Promise = global.Promise;
 mongoose.connect(dbUri)
