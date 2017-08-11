afd724d8120d4c87af2f00dc7e8bc73d60a59f98
ChengFu Lin <chengfulin0806@gmail.com>
2017-08-11 18:33:11 +0800

    make secret

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const linebot = require('linebot');
 const bodyParser = require("body-parser");
 
 const bot = linebot({
-    channelId: '1517058705',
-    channelSecret: 'f2b0bd7ed2faf7f3fcddc58a67306561',
-    channelAccessToken: 'poBOF4jaxKKd8zfQjz79UqBNK1U69117TE1d80T75cE1G8ZYKeS/5pdGSqoanGNppBP717mlFuE/TE0sDvs6qER0Y7Uwt1TH/oouJ0VFMKGM6cysqnDjLL0Do1hgT9FjrqkLkp0q4YLj8SUWw3KY6wdB04t89/1O/w1cDnyilFU=',
+    channelId: '[channel id]',
+    channelSecret: '[channel secret]',
+    channelAccessToken: '[channel access token]',
     verify: true // Verify 'X-Line-Signature' header (default=true) 
 });
 
